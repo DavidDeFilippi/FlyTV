8981d438ca30172a86674f5e48e13771e9e5df28
DavidDeFilippi <daviddefilippi@gmail.com>
2024-05-22 10:51:45 -0400

    ATV Experimental two menus

--- src/app/dplayer/dplayer.page.ts
+++ src/app/dplayer/dplayer.page.ts
@@ -7,7 +7,7 @@ import { IonRouterOutlet, Platform, AlertController } from '@ionic/angular';
 import { AppLauncher } from '@capacitor/app-launcher';
 
 declare var VideoHls: any;
-declare var setMenuActive: any;
+
 @Component({
   selector: 'app-dplayer',
   templateUrl: './dplayer.page.html',
@@ -19,7 +19,6 @@ export class DplayerPage implements OnInit {
       text: 'NO',
       role: 'cancel',
       handler: () => {
-        console.log('Alert canceled');
       },
     },
     {
@@ -27,21 +26,31 @@ export class DplayerPage implements OnInit {
       role: 'confirm',
       handler: () => {
         App.exitApp();
-        console.log('Alert confirmed');
       },
     },
   ];
 
   isMobile: boolean = false;
-  channels: any;
+  channels: any = [];
+  channelsFull: any;
   premium: any;
   nacionales: any;
   entretencion: any;
   noticias: any;
+  category: any
+  categories: any;
   setTimeoutMenu: any;
   clickPlay: boolean = false;
   menuIsActive: boolean = false;
   channeIinfo: any;
+  intervalTransmitiendo: any;
+  diaParrilla: string = '';
+  diaNombreyMesParrilla: any;
+  nombreDias = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+  nombreMeses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+  isMenuLeft: boolean = false;
+  isMenuMiddle: boolean = true;
+  isMenuRight: boolean = false;
 
   @ViewChild(IonRouterOutlet) outlet: any;
   constructor(
@@ -50,7 +59,6 @@ export class DplayerPage implements OnInit {
     private channelService: ChannelsService,
     private alertController: AlertController,
     private platform: Platform,
-
   ) {
     this.platform.backButton.subscribeWithPriority(9, () => {
       if (!this.outlet?.canGoBack()) {
@@ -75,34 +83,99 @@ export class DplayerPage implements OnInit {
 
   }
 
+  ionViewWillLeave() {
+    clearInterval(this.intervalTransmitiendo);
+  }
+
   getChannels() {
     this.channelService.getChannels().subscribe((data) => {
-      this.channels = data;
-      this.nacionales = this.channels.filter(function(c: any){
-        return c.categoria === 'nacionales';
-      });
-      this.premium = this.channels.filter(function(c: any){
-        return c.categoria === 'premium';
-      });
-      this.noticias = this.channels.filter(function(c: any){
-        return c.categoria === 'noticias';
-      });
-      this.menuIsActive = true;
+      this.channelsFull = data;
+      // this.nacionales = this.channels.filter(function(c: any){
+      //   return c.categoria === 'nacionales';
+      // });
+      // this.premium = this.channels.filter(function(c: any){
+      //   return c.categoria === 'premium';
+      // });
+      // this.noticias = this.channels.filter(function(c: any){
+      //   return c.categoria === 'noticias';
+      // });
+      this.listCategories();
+      this.getCategory();
+      this.getTransmitiendo();
       this.getLastChannel();
-      this.setMenuActive();
 
     });
   }
 
   playChannel(ch: any) {
-    if (this.menuIsActive) {
-      this.channeIinfo = ch;
-      clearTimeout(this.setTimeoutMenu);
-      this.setMenuActive();
-      new VideoHls('', 'stop', this.isMobile, 'videoDesktop');
-      new VideoHls(ch.url, 'play', this.isMobile, 'videoDesktop');
-      localStorage.setItem('lastchannel', ch.id);
+    this.channeIinfo = ch;
+    clearTimeout(this.setTimeoutMenu);
+    new VideoHls('', 'stop', this.isMobile, 'videoDesktop');
+    new VideoHls(ch.url, 'play', this.isMobile, 'videoDesktop');
+    localStorage.setItem('lastchannel', ch.id);
+    this.isMenuLeft = false;
+    this.isMenuMiddle = true;
+    this.isMenuRight = false;
+  }
+
+  async getTransmitiendo() {
+    clearInterval(this.intervalTransmitiendo);
+    for (let i = 0; i < this.channelsFull.length; i++) {
+      if (this.channelsFull[i].parrilla) {
+        this.channelsFull[i].transmitiendo.current = this.getCurrentPrograma(this.channelsFull[i].parrilla);
+        this.channelsFull[i].transmitiendo.next = this.getNextPrograma(this.channelsFull[i].parrilla);
+      }
     }
+
+    this.intervalTransmitiendo = setInterval(() => {
+      for (let i = 0; i < this.channels.length; i++) {
+        if (this.channelsFull[i].parrilla) {
+          this.channelsFull[i].transmitiendo.current = this.getCurrentPrograma(this.channelsFull[i].parrilla);
+          this.channelsFull[i].transmitiendo.next = this.getNextPrograma(this.channelsFull[i].parrilla);
+        }
+      }
+    }, 30000);
+  }
+
+  // :::::::::::::DETERMINA QUE PROGRAMA SE ESTA TRANSMITIENDO EN UN CANAL::::::::::::::::
+  getCurrentPrograma(x: any) {
+    let y: string = '';
+    let today = new Date();
+
+    for (let i = 0; i < x.length; i++) {
+
+      const inicio = new Date(x[i].hora);
+      let fin;
+      if (i < x.length - 1) {
+        fin = new Date(x[i + 1].hora);
+      } else {
+        fin = new Date().setHours(today.getHours() + 1);
+      }
+
+      if (today > inicio && today < fin) {
+        y = x[i].horaNormal + ' ' + x[i].programa + '';
+      }
+
+    }
+    return y;
+  }
+
+  // :::::::::::::DETERMINA QUE PROGRAMA SE TRANSMITIRA DESPUES EN UN CANAL::::::::::::::::
+  getNextPrograma(x: any) {
+    let y: string = '';
+    let today = new Date();
+
+    for (let i = 0; i < x.length - 1; i++) {
+      const inicio = new Date(x[i].hora);
+      const fin = new Date(x[i + 1].hora);
+
+      if (today > inicio && today < fin) {
+        y = x[i + 1].horaNormal + ' ' + x[i + 1].programa + '';
+      }
+    }
+    this.diaParrilla = new Date(x[0].hora).toLocaleDateString();
+    this.diaNombreyMesParrilla = { dia: this.nombreDias[new Date(x[0].hora).getDay()], numeroDia: new Date(x[0].hora).getDate(), mes: this.nombreMeses[new Date(x[0].hora).getMonth()], year: new Date(x[0].hora).getFullYear() };
+    return y;
   }
 
   getLastChannel() {
@@ -114,32 +187,82 @@ export class DplayerPage implements OnInit {
       id = localStorage.getItem('lastchannel');
     }
     localStorage.setItem('lastchannel', id);
-    for (let i = 0; i < this.channels.length; i++) {
-      if (this.channels[i].id == id) {
-        ch = this.channels[i];
-        this.channeIinfo = this.channels[i];
+    for (let i = 0; i < this.channelsFull.length; i++) {
+      if (this.channelsFull[i].id == id) {
+        ch = this.channelsFull[i];
+        this.channeIinfo = this.channelsFull[i];
         break;
       }
     }
     this.playChannel(ch);
   }
 
-  setMenuActive() {
-    clearTimeout(this.setTimeoutMenu);
-    this.menuIsActive = true;
-    new setMenuActive(true);
-    this.setTimeoutMenu = setTimeout(() => {
-      new setMenuActive(false);
-      this.menuIsActive = false;
-    }, 3000);
+  // ::::::::::::LISTA TODAS LAS CATEGORIAS DE LOS CANALES DISPONIBLES::::::::
+  async listCategories() {
+
+    this.categories = [];
+    for (let i = 0; i < this.channelsFull.length; i++) {
+      this.categories.push(this.channelsFull[i].categoria);
+    }
+
+    this.categories = [...new Set(this.categories)];
+  }
+
+  getCategory(category?: string) {
+    category = !category || category === '' ? 'nacionales' : category;
+
+    this.channels = [];
+
+    for (let i = 0; i < this.channelsFull.length; i++) {
+      if (this.channelsFull[i].categoria === category) {
+        this.channels.push(this.channelsFull[i]);
+      }
+    }
+
+    this.isMenuLeft = true;
+    this.isMenuMiddle = false;
+    this.isMenuRight = false;
+
   }
 
   @HostListener('document:keydown', ['$event'])
   onKeyDown(event: KeyboardEvent) {
     if ((event.key === 'ArrowLeft' || event.key === 'ArrowRight' || event.key === 'ArrowUp' || event.key === 'ArrowDown' || event.key === 'Enter') && !this.globalVar.getExitDialog()) {
       event.stopImmediatePropagation();
+
+      if (event.key === 'ArrowRight') {
+
+        if (this.isMenuMiddle) {
+          this.isMenuLeft = false;
+          this.isMenuMiddle = false;
+          this.isMenuRight = true;
+        }
+
+        if (this.isMenuLeft) {
+          this.isMenuLeft = false;
+          this.isMenuMiddle = true;
+          this.isMenuRight = false;
+        }
+
+      } else {
+        event.stopPropagation();
+      }
+
       if (event.key === 'ArrowLeft') {
-        this.setMenuActive();
+
+
+        if (this.isMenuMiddle) {
+          this.isMenuLeft = true;
+          this.isMenuMiddle = false;
+          this.isMenuRight = false;
+        }
+
+        if (this.isMenuRight) {
+          this.isMenuLeft = false;
+          this.isMenuMiddle = true;
+          this.isMenuRight = false;
+        }
+
       } else {
         event.stopPropagation();
       }
@@ -162,12 +285,10 @@ export class DplayerPage implements OnInit {
 
   //::::::::PARA COMPARTIR CON APPS DE VIDEO EXTERNAS (SIRVE OPCIONALMENTE PARA URL DE VIDEOS HTTP):::::::::
   async openChannelUrl(url: string) {
-    if (this.menuIsActive) {
       const canopen = await AppLauncher.canOpenUrl({ url: 'org.videolan.vlc' });
       if (canopen.value) {
         window.open('vlc://' + url, "_blank");
       }
-    }
   }
 
 }
